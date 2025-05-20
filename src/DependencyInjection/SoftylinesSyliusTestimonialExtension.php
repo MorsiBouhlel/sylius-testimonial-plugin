6f4f63ba58078ff04ebb04e6fabdccbd9084f2d7
Morsi BOUHLEL <morsi.bouhlel@dotit-medu.com>
2025-05-20 09:44:00 +0100

    updates

--- src/DependencyInjection/SoftylinesSyliusTestimonialExtension.php
+++ src/DependencyInjection/SoftylinesSyliusTestimonialExtension.php
@@ -18,7 +18,6 @@ final class SoftylinesSyliusTestimonialExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.yaml');
-        $loader->load('gaufrette.yaml');
     }
 
     public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
